1ce74e7508184e4813e640edda229810830ded1e
Hüseyin Kabil <hsynkabil@gmail.com>
2016-09-23 15:15:35 +0300

    update version on setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='hotelspro-client',
-    version='0.0.2',
+    version='0.0.1',
     packages=find_packages(),
     url='https://github.com/huseyinkabil/hotelspro-client',
     license='MIT',
