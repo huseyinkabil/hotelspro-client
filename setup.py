1d719bc038400578c12d71b7ee12f7ecfdc48e90
Hüseyin Kabil <hsynkabil@gmail.com>
2016-09-23 15:13:59 +0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hotelspro-client',
+    version='0.0.2',
+    packages=find_packages(),
+    url='https://github.com/huseyinkabil/hotelspro-client',
+    license='MIT',
+    author='Hüseyin Kabil',
+    author_email='hsynkabil@gmail.com',
+    description='Hotelspro.com api client',
+    install_requires=['requests'],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
